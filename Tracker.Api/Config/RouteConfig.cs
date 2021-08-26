2be7b1d33e9e3146b5f4a48ef3b5380beaa498d0
Koki <Koki@KOKETSO>
2021-08-26 15:11:14 +0200

    r1

--- Tracker.Api/Config/RouteConfig.cs
+++ Tracker.Api/Config/RouteConfig.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tracker.Api
+namespace Tracker.Api.Config
 {
-    public class Class1
+    class RouteConfig
     {
     }
 }
